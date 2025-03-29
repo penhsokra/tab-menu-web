f7bf33c05fd13b0311126007dd24a437f09a9665
penhsokra <penhsokra@gmail.com>
2025-03-30 03:26:20 +0900

    ax

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2806275702"
+    serviceWorkerVersion: "2659932570"
   }
 });
